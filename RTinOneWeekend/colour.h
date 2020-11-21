9fda4a8e2a6968505252e06db69a91000ed01f6f
FergusBrown <fergus.brown123@gmail.com>
2020-11-21 15:19:59 +0000

    Add project files.

---
+++ RTinOneWeekend/colour.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "vec3.h"
+
+#include <iostream>
+
+void write_colour(std::ostream& out, colour pixel_colour)
+{
+	// Write the translated [0, 255] calue of each colour component
+	out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
+		<< static_cast<int>(255.999 * pixel_colour.y()) << ' '
+		<< static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+}
